82b99ac6890e238eae6bf30eae08307cddaa964b
Johnnie Yu <7027377+johnnieyu@users.noreply.github.com>
2025-06-24 02:23:24 +0200

    added tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -8,13 +9,12 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Johnnie Yu - Developer & Designer",
-  description: "Versatile cross-functional developer, dedicated to crafting & refining digital narratives alongside various innovative tools.",
-  keywords: ["developer", "designer", "portfolio", "nextjs", "react"],
+  title: "Listen.co(des)",
+  description: "Listen.codes is a project led by Johnnie Yu, tech-ifying the combined knowledge of Listen into a series of tech products, sometimes useful, mostly experimental.",
   authors: [{ name: "Johnnie Yu" }],
   openGraph: {
-    title: "Johnnie Yu - Developer & Designer",
-    description: "Versatile cross-functional developer, dedicated to crafting & refining digital narratives alongside various innovative tools.",
+    title: "Listen.co(des)",
+    description: "Listen.codes is a project led by Johnnie Yu, tech-ifying the combined knowledge of Listen into a series of tech products, sometimes useful, mostly experimental.",
     type: "website",
   },
 };
@@ -26,6 +26,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://umami.listen.codes/script.js"
+          data-website-id="737cc6af-7158-407a-a868-b7b07d8dd0e2"
+          strategy="afterInteractive"
+          defer
+        />
+      </head>
       <body className={`${inter.variable} antialiased`}>
         {children}
       </body>
